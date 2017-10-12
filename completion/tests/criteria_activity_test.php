e80c8f76d7fcc8d5f0337cd2dfc686fd22fcd092
Frédéric Massart <fred@branchup.tech>
2017-10-12 18:06:15 +0800

    MDL-56453 core_completion: Adding tests covering successful completion

--- completion/tests/criteria_activity_test.php
+++ completion/tests/criteria_activity_test.php
@@ -23,6 +23,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+use core_completion\api;
+
 defined('MOODLE_INTERNAL') || die();
 
 global $CFG;
@@ -37,21 +39,54 @@ require_once($CFG->dirroot . '/mod/assign/locallib.php');
  * @category   test
  * @copyright  2017 Frédéric Massart <fred@branchup.tech>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @coversDefaultClass \completion_criteria_activity
  */
-class core_completion_criteria_activity_testcase extends advanced_testcase {
+class completion_criteria_activity_test extends advanced_testcase {
 
-    public function setUp() {
+    public function setUp(): void {
         global $CFG;
         $CFG->enablecompletion = true;
         $this->resetAfterTest();
     }
 
     /**
+     * Data provider for review test.
+     *
+     * @return array
+     */
+    public static function review_provider(): array {
+        return [
+            [
+                [
+                    api::STATUS_COMPLETED,
+                    api::STATUS_COMPLETED,
+                    api::STATUS_COMPLETED,
+                    api::STATUS_COMPLETED,
+                ],
+                [true, true, true, true]
+            ],
+            [
+                [
+                    api::STATUS_COMPLETED_PASS,
+                    api::STATUS_COMPLETED_PASS,
+                    api::STATUS_COMPLETED_PASS,
+                    api::STATUS_COMPLETED_PASS,
+                ],
+                [true, true, true, false]
+            ],
+        ];
+    }
+
+    /**
      * Tests the review method.
      *
+     * @dataProvider review_provider
+     * @param array $statusset The status to set to the critaria.
+     * @param array $expected The expected completion after processing the criteria.
      * @return void
+     * @covers ::review
      */
-    public function test_review() {
+    public function test_review($statusset, $expected): void {
         $this->setAdminUser();
         $dg = $this->getDataGenerator();
 
@@ -77,10 +112,10 @@ class core_completion_criteria_activity_testcase extends advanced_testcase {
         $critdata = new stdClass();
         $critdata->id = $course->id;
         $critdata->criteria_activity = [
-            $assign1->cmid => 1,
-            $assign2->cmid => 1,
-            $assign3->cmid => 1,
-            $assign4->cmid => 1
+            $assign1->cmid => $statusset[0],
+            $assign2->cmid => $statusset[1],
+            $assign3->cmid => $statusset[2],
+            $assign4->cmid => $statusset[3]
         ];
         $criterion = new completion_criteria_activity();
         $criterion->update_config($critdata);
@@ -131,32 +166,64 @@ class core_completion_criteria_activity_testcase extends advanced_testcase {
         $gi->update_final_grade($student->id, 69);
 
         // Now should be complete, but we did not mark it so it's still incomplete in the completion statuses.
-        $this->assertTrue($crit1->review($complcrit1, false));
-        $this->assertTrue($crit2->review($complcrit2, false));
-        $this->assertTrue($crit3->review($complcrit3, false));
-        $this->assertTrue($crit4->review($complcrit4, false));
+        $this->assertEquals($expected[0], $crit1->review($complcrit1, false));
+        $this->assertEquals($expected[1], $crit2->review($complcrit2, false));
+        $this->assertEquals($expected[2], $crit3->review($complcrit3, false));
+        $this->assertEquals($expected[3], $crit4->review($complcrit4, false));
         $this->assertFalse($complcrit1->is_complete());
         $this->assertFalse($complcrit2->is_complete());
         $this->assertFalse($complcrit3->is_complete());
         $this->assertFalse($complcrit4->is_complete());
 
         // Now we shoudl be writing in the completion status.
-        $this->assertTrue($crit1->review($complcrit1, true));
-        $this->assertTrue($crit2->review($complcrit2, true));
-        $this->assertTrue($crit3->review($complcrit3, true));
-        $this->assertTrue($crit4->review($complcrit4, true));
-        $this->assertTrue($complcrit1->is_complete());
-        $this->assertTrue($complcrit2->is_complete());
-        $this->assertTrue($complcrit3->is_complete());
-        $this->assertTrue($complcrit4->is_complete());
+        $this->assertEquals($expected[0], $crit1->review($complcrit1, true));
+        $this->assertEquals($expected[1], $crit2->review($complcrit2, true));
+        $this->assertEquals($expected[2], $crit3->review($complcrit3, true));
+        $this->assertEquals($expected[3], $crit4->review($complcrit4, true));
+        $this->assertEquals($expected[0], $complcrit1->is_complete());
+        $this->assertEquals($expected[1], $complcrit2->is_complete());
+        $this->assertEquals($expected[2], $complcrit3->is_complete());
+        $this->assertEquals($expected[3], $complcrit4->is_complete());
+    }
+
+    /**
+     * Data provider for review test.
+     *
+     * @return array
+     */
+    public static function cron_provider() {
+        return [
+            [
+                [
+                    api::STATUS_COMPLETED,
+                    api::STATUS_COMPLETED,
+                    api::STATUS_COMPLETED,
+                    api::STATUS_COMPLETED,
+                ],
+                [true, true, true, true]
+            ],
+            [
+                [
+                    api::STATUS_COMPLETED_PASS,
+                    api::STATUS_COMPLETED_PASS,
+                    api::STATUS_COMPLETED_PASS,
+                    api::STATUS_COMPLETED_PASS,
+                ],
+                [true, true, true, false]
+            ],
+        ];
     }
 
     /**
      * Tests the review method.
      *
+     * @dataProvider review_provider
+     * @param array $statusset The status to set to the critaria.
+     * @param array $expected The expected completion after processing the criteria.
      * @return void
+     * @covers ::cron
      */
-    public function test_cron() {
+    public function test_cron($statusset, $expected) {
         $this->setAdminUser();
         $dg = $this->getDataGenerator();
 
@@ -182,10 +249,10 @@ class core_completion_criteria_activity_testcase extends advanced_testcase {
         $critdata = new stdClass();
         $critdata->id = $course->id;
         $critdata->criteria_activity = [
-            $assign1->cmid => 1,
-            $assign2->cmid => 1,
-            $assign3->cmid => 1,
-            $assign4->cmid => 1
+            $assign1->cmid => $statusset[0],
+            $assign2->cmid => $statusset[1],
+            $assign3->cmid => $statusset[2],
+            $assign4->cmid => $statusset[3]
         ];
         $criterion = new completion_criteria_activity();
         $criterion->update_config($critdata);
@@ -256,9 +323,9 @@ class core_completion_criteria_activity_testcase extends advanced_testcase {
             'course' => $course->id,
             'criteriaid' => $crit4->id,
         ]);
-        $this->assertTrue($complcrit1->is_complete());
-        $this->assertTrue($complcrit2->is_complete());
-        $this->assertTrue($complcrit3->is_complete());
-        $this->assertTrue($complcrit4->is_complete());
+        $this->assertEquals($expected[0], $complcrit1->is_complete());
+        $this->assertEquals($expected[1], $complcrit2->is_complete());
+        $this->assertEquals($expected[2], $complcrit3->is_complete());
+        $this->assertEquals($expected[3], $complcrit4->is_complete());
     }
 }
