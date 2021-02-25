c736bd3a9edb8104e97df16faddc37ddfcc29fdd
Andrew Nicols <andrew@nicols.co.uk>
2021-02-25 10:41:25 +0800

    MDL-70990 core_block: Replace legacy BLOCK_CONTENT_UPDATED event
    The legacy M.core.event.BLOCK_CONTENT_UPDATED event has been replaced with a
    new core_block/events::blockContentUpdated native DOM event.
    
    The new event can be triggered using the `notifyBlockContentUpdated`
    event, and by providing the HTMLElement that was updated, for example:
    
    ```
    import {notifyBlockContentUpdated} from 'core_block/events';
    
    const someHandler = e => {
        // ...
        const updatedBlock = e.target.closest('.block');
        notifyBlockContentUpdated(updatedBlock);
    };
    ```
    
    The new event can be listened to at any point in the DOM using the
    following syntax:
    
    ```
    import {eventTypes} from 'core_block/events';
    
    const handler = e => {
        // The block that was updated.
        e.target;
    
        // The id of the updated block.
        e.detail.instanceId;
    };
    
    document.addEventListener(eventTypes.blockContentUpdated, handler);
    ```
    
    A backward-compatabibility layer is included to ensure that any legacy
    YUI event listener is still called with the same arguments.
    
    This legacy bridges will be removed in Moodle 4.3.

--- blocks/navigation/amd/src/navblock.js
+++ blocks/navigation/amd/src/navblock.js
@@ -21,26 +21,20 @@
  * @copyright  2015 John Okely <john@moodle.com>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-define(['jquery', 'core/tree'], function($, Tree) {
-    return {
-        init: function(instanceid) {
-            var navTree = new Tree(".block_navigation .block_tree");
-            navTree.finishExpandingGroup = function(item) {
-                Tree.prototype.finishExpandingGroup.call(this, item);
-                Y.use('moodle-core-event', function() {
-                    Y.Global.fire(M.core.globalEvents.BLOCK_CONTENT_UPDATED, {
-                        instanceid: instanceid
-                    });
-                });
-            };
-            navTree.collapseGroup = function(item) {
-                Tree.prototype.collapseGroup.call(this, item);
-                Y.use('moodle-core-event', function() {
-                    Y.Global.fire(M.core.globalEvents.BLOCK_CONTENT_UPDATED, {
-                        instanceid: instanceid
-                    });
-                });
-            };
-        }
+import {notifyBlockContentUpdated} from 'core_block/events';
+import Tree from 'core/tree';
+
+export const init = instanceId => {
+    const navTree = new Tree(".block_navigation .block_tree");
+    const blockNode = document.querySelector(`[data-instance-id="${instanceId}"]`);
+
+    navTree.finishExpandingGroup = item => {
+        Tree.prototype.finishExpandingGroup.call(navTree, item);
+        notifyBlockContentUpdated(blockNode);
     };
-});
+
+    navTree.collapseGroup = item => {
+        Tree.prototype.collapseGroup.call(navTree, item);
+        notifyBlockContentUpdated(blockNode);
+    };
+};
