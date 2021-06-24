c1cf78a66ece97d477139ad75333b28be399a337
Andrew Nicols <andrew@nicols.co.uk>
2021-06-24 11:45:40 +0800

    MDL-71998 core: Confirm should use GET

--- lib/outputrenderers.php
+++ lib/outputrenderers.php
@@ -1986,9 +1986,9 @@ class core_renderer extends renderer_base {
             // ok
             $continue->primary = true;
         } else if (is_string($continue)) {
-            $continue = new single_button(new moodle_url($continue), get_string('continue'), 'post', true);
+            $continue = new single_button(new moodle_url($continue), get_string('continue'), 'get', true);
         } else if ($continue instanceof moodle_url) {
-            $continue = new single_button($continue, get_string('continue'), 'post', true);
+            $continue = new single_button($continue, get_string('continue'), 'get', true);
         } else {
             throw new coding_exception('The continue param to $OUTPUT->confirm() must be either a URL (string/moodle_url) or a single_button instance.');
         }
