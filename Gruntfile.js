c3fef57b6f241cb318e409c12c64e7622fccca43
Andrew Nicols <andrew@nicols.co.uk>
2022-02-18 08:57:51 +0800

    MDL-73915 js: Disable amd variable mangling
    Unfortunately the babel minify-mangle plugin seems to be abandoned and
    in certain circumstances can be buggy. The only safe option is to
    disable it.
    
    Since the upgrade to Node LTS/Gallium requires an upgrade to @babel/core
    and eslint, which change the built files anyway, this seems like the
    ideal time to make this change.
    
    This does lead to slightly larger files, but with a much lower chance of
    errors due to the mangling.

--- Gruntfile.js
+++ Gruntfile.js
@@ -325,7 +325,8 @@ module.exports = function(grunt) {
                         // This minification plugin needs to be disabled because it breaks the
                         // source map generation and causes invalid source maps to be output.
                         simplify: false,
-                        builtIns: false
+                        builtIns: false,
+                        mangle: false,
                     }],
                     ['@babel/preset-env', {
                         targets: {
