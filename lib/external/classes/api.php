1c0ea0c86e93e84f7a1342c0199290c192f75ef8
Andrew Nicols <andrew@nicols.co.uk>
2022-12-07 11:49:40 +0800

    MDL-76583 core_external: Migrate external_description class

--- lib/external/classes/api.php
+++ lib/external/classes/api.php
@@ -26,7 +26,6 @@ use invalid_parameter_exception;
 use invalid_response_exception;
 use moodle_exception;
 use restricted_context_exception;
-use external_description;
 use external_function_parameters;
 use external_value;
 use external_multiple_structure;
