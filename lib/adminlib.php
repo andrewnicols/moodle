ad5587295dcd110208331330bcfc2c620c1387c1
Andrew Nicols <andrew@nicols.co.uk>
2022-12-27 21:03:05 +0800

    MDL-76758 core_admin: Re-home admin_root

--- lib/adminlib.php
+++ lib/adminlib.php
@@ -663,70 +663,10 @@ function enable_cli_maintenance_mode() {
 
 /// CLASS DEFINITIONS /////////////////////////////////////////////////////////
 
+class_alias(\core_admin\local\tree\category::class, 'admin_category');
 class_alias(\core_admin\local\tree\part_of_admin_tree::class, 'part_of_admin_tree');
 class_alias(\core_admin\local\tree\parentable_part_of_admin_tree::class, 'parentable_part_of_admin_tree');
-class_alias(\core_admin\local\tree\category::class, 'admin_category');
-
-/**
- * Root of admin settings tree, does not have any parent.
- *
- * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-class admin_root extends admin_category {
-/** @var array List of errors */
-    public $errors;
-    /** @var string search query */
-    public $search;
-    /** @var bool full tree flag - true means all settings required, false only pages required */
-    public $fulltree;
-    /** @var bool flag indicating loaded tree */
-    public $loaded;
-    /** @var mixed site custom defaults overriding defaults in settings files*/
-    public $custom_defaults;
-
-    /**
-     * @param bool $fulltree true means all settings required,
-     *                            false only pages required
-     */
-    public function __construct($fulltree) {
-        global $CFG;
-
-        parent::__construct('root', get_string('administration'), false);
-        $this->errors   = array();
-        $this->search   = '';
-        $this->fulltree = $fulltree;
-        $this->loaded   = false;
-
-        $this->category_cache = array();
-
-        // load custom defaults if found
-        $this->custom_defaults = null;
-        $defaultsfile = "$CFG->dirroot/local/defaults.php";
-        if (is_readable($defaultsfile)) {
-            $defaults = array();
-            include($defaultsfile);
-            if (is_array($defaults) and count($defaults)) {
-                $this->custom_defaults = $defaults;
-            }
-        }
-    }
-
-    /**
-     * Empties children array, and sets loaded to false
-     *
-     * @param bool $requirefulltree
-     */
-    public function purge_children($requirefulltree) {
-        $this->children = array();
-        $this->fulltree = ($requirefulltree || $this->fulltree);
-        $this->loaded   = false;
-        //break circular dependencies - this helps PHP 5.2
-        while($this->category_cache) {
-            array_pop($this->category_cache);
-        }
-        $this->category_cache = array();
-    }
-}
+class_alias(\core_admin\local\tree\root::class, 'admin_root');
 
 
 /**
