884d052f8c2031e3a7d12d765c84166bdfcd93e5
Andrew Nicols <andrew@nicols.co.uk>
2022-12-27 20:51:04 +0800

    MDL-76758 core_admin: Re-home part_of_admin_tree

--- lib/adminlib.php
+++ lib/adminlib.php
@@ -663,78 +663,7 @@ function enable_cli_maintenance_mode() {
 
 /// CLASS DEFINITIONS /////////////////////////////////////////////////////////
 
-
-/**
- * Interface for anything appearing in the admin tree
- *
- * The interface that is implemented by anything that appears in the admin tree
- * block. It forces inheriting classes to define a method for checking user permissions
- * and methods for finding something in the admin tree.
- *
- * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-interface part_of_admin_tree {
-
-/**
- * Finds a named part_of_admin_tree.
- *
- * Used to find a part_of_admin_tree. If a class only inherits part_of_admin_tree
- * and not parentable_part_of_admin_tree, then this function should only check if
- * $this->name matches $name. If it does, it should return a reference to $this,
- * otherwise, it should return a reference to NULL.
- *
- * If a class inherits parentable_part_of_admin_tree, this method should be called
- * recursively on all child objects (assuming, of course, the parent object's name
- * doesn't match the search criterion).
- *
- * @param string $name The internal name of the part_of_admin_tree we're searching for.
- * @return mixed An object reference or a NULL reference.
- */
-    public function locate($name);
-
-    /**
-     * Removes named part_of_admin_tree.
-     *
-     * @param string $name The internal name of the part_of_admin_tree we want to remove.
-     * @return bool success.
-     */
-    public function prune($name);
-
-    /**
-     * Search using query
-     * @param string $query
-     * @return mixed array-object structure of found settings and pages
-     */
-    public function search($query);
-
-    /**
-     * Verifies current user's access to this part_of_admin_tree.
-     *
-     * Used to check if the current user has access to this part of the admin tree or
-     * not. If a class only inherits part_of_admin_tree and not parentable_part_of_admin_tree,
-     * then this method is usually just a call to has_capability() in the site context.
-     *
-     * If a class inherits parentable_part_of_admin_tree, this method should return the
-     * logical OR of the return of check_access() on all child objects.
-     *
-     * @return bool True if the user has access, false if she doesn't.
-     */
-    public function check_access();
-
-    /**
-     * Mostly useful for removing of some parts of the tree in admin tree block.
-     *
-     * @return True is hidden from normal list view
-     */
-    public function is_hidden();
-
-    /**
-     * Show we display Save button at the page bottom?
-     * @return bool
-     */
-    public function show_save();
-}
-
+class_alias(\core_admin\local\tree\part_of_admin_tree::class, 'part_of_admin_tree');
 
 /**
  * Interface implemented by any part_of_admin_tree that has children.
