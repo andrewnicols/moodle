6b5d709b0f0e74da01d26272c6dc429a935f9201
Andrew Nicols <andrew@nicols.co.uk>
2023-01-23 22:04:29 +0800

    MDL-76231 block_recentlyaccessitems: AMD to ESM migration

--- blocks/recentlyaccesseditems/amd/src/main.js
+++ blocks/recentlyaccesseditems/amd/src/main.js
@@ -22,111 +22,80 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-define(
-    [
-        'jquery',
-        'block_recentlyaccesseditems/repository',
-        'core/templates',
-        'core/notification'
-    ],
-    function(
-        $,
-        Repository,
-        Templates,
-        Notification
-    ) {
+import $ from 'jquery';
+import {getRecentItems} from './repository';
+import Notification from 'core/notification';
+import Templates from 'core/templates';
 
-        var NUM_ITEMS = 9;
-        // Maximum number of elements to display in the block initially.
-        var NUM_ITEMS_INIT = 3;
 
-        var SELECTORS = {
-            CARDDECK_CONTAINER: '[data-region="recentlyaccesseditems-view"]',
-            CARDDECK: '[data-region="recentlyaccesseditems-view-content"]',
-            SHOWMORE_LINK: '[data-region="recentlyaccesseditems-view"] [data-action="more-items"]',
-        };
+const NUM_ITEMS = 9;
+// Maximum number of elements to display in the block initially.
+const NUM_ITEMS_INIT = 3;
 
-        /**
-         * Register event listeners.
-         */
-        const registerEventListeners = () => {
-            const showmoreLink = document.querySelector(SELECTORS.SHOWMORE_LINK);
+const Selectors = {
+    CARDDECK_CONTAINER: '[data-region="recentlyaccesseditems-view"]',
+    CARDDECK: '[data-region="recentlyaccesseditems-view-content"]',
+    SHOWMORE_LINK: '[data-region="recentlyaccesseditems-view"] [data-action="more-items"]',
+};
 
-            // Hide "Show more" link and show additional items.
-            showmoreLink.addEventListener('click', () => {
-                showmoreLink.classList.add('d-none');
+/**
+ * Register event listeners.
+ */
+const registerEventListeners = () => {
+    const showmoreLink = document.querySelector(Selectors.SHOWMORE_LINK);
 
-                const hiddenItems = document.querySelector('[data-region="items-list"]').children;
-                hiddenItems.forEach(function(hiddenItem) {
-                    hiddenItem.style = "display: block";
-                });
-            });
-        };
+    // Hide "Show more" link and show additional items.
+    showmoreLink.addEventListener('click', () => {
+        showmoreLink.classList.add('d-none');
 
-        /**
-         * Get recent items from backend.
-         *
-         * @method getRecentItems
-         * @param {int} limit Only return this many results
-         * @return {array} Items user most recently has accessed
-         */
-        var getRecentItems = function(limit) {
-            return Repository.getRecentItems(limit);
-        };
-
-        /**
-         * Render the block content.
-         *
-         * @method renderItems
-         * @param {object} root The root element for the items view.
-         * @param {array} items containing array of returned items.
-         * @return {promise} Resolved with HTML and JS strings
-         */
-        var renderItems = function(root, items) {
-            if (items.length > 0) {
-                let hasmoreitems = false;
-                if (items.length > NUM_ITEMS_INIT) {
-                    hasmoreitems = true;
-                }
-                return Templates.render('block_recentlyaccesseditems/view-cards', {
-                    items: items,
-                    hasmoreitems: hasmoreitems
-                });
-            } else {
-                var noitemsimgurl = root.attr('data-noitemsimgurl');
-                return Templates.render('block_recentlyaccesseditems/no-items', {
-                    noitemsimgurl: noitemsimgurl
-                });
-            }
-        };
-
-        /**
-         * Get and show the recent items into the block.
-         *
-         * @param {object} root The root element for the items block.
-         */
-        var init = function(root) {
-            root = $(root);
-
-            var itemsContainer = root.find(SELECTORS.CARDDECK_CONTAINER);
-            var itemsContent = root.find(SELECTORS.CARDDECK);
-
-            var itemsPromise = getRecentItems(NUM_ITEMS);
-
-            itemsPromise.then(function(items) {
-                var pageContentPromise = renderItems(itemsContainer, items);
-
-                pageContentPromise.then(function(html, js) {
-                    Templates.replaceNodeContents(itemsContent, html, js);
-                    if (items.length > 3) {
-                        registerEventListeners();
-                    }
-                    return null;
-                }).catch(Notification.exception);
-            }).catch(Notification.exception);
-        };
-
-        return {
-            init: init
-        };
+        const hiddenItems = document.querySelector('[data-region="items-list"]').children;
+        hiddenItems.forEach(function(hiddenItem) {
+            hiddenItem.style = "display: block";
+        });
     });
+};
+
+/**
+ * Render the block content.
+ *
+ * @method renderItems
+ * @param {object} root The root element for the items view.
+ * @param {array} items containing array of returned items.
+ * @return {promise} Resolved with HTML and JS strings
+ */
+const renderItems = function(root, items) {
+    if (items.length > 0) {
+        return Templates.renderForPromise('block_recentlyaccesseditems/view-cards', {
+            items,
+            hasmoreitems: (items.length > NUM_ITEMS_INIT),
+        });
+    } else {
+        return Templates.renderForPromise('block_recentlyaccesseditems/no-items', {
+            noitemsimgurl: root.attr('data-noitemsimgurl'),
+        });
+    }
+};
+
+/**
+ * Get and show the recent items into the block.
+ *
+ * @param {object} root The root element for the items block.
+ */
+export const init = async (root) =>{
+    root = $(root);
+
+    const itemsContainer = root.find(Selectors.CARDDECK_CONTAINER);
+    const itemsContent = root.find(Selectors.CARDDECK);
+
+    try {
+        const items = await getRecentItems(NUM_ITEMS);
+        const {html, js} = await renderItems(itemsContainer, items);
+
+        Templates.replaceNodeContents(itemsContent, html, js);
+        if (items.length > 3) {
+            registerEventListeners();
+        }
+    } catch (error) {
+        Notification.exception(error);
+    }
+};
--- blocks/recentlyaccesseditems/amd/src/repository.js
+++ blocks/recentlyaccesseditems/amd/src/repository.js
@@ -20,27 +20,24 @@
  * @copyright  2018 Victor Deniz <victor@moodle.com>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-define(['core/ajax'], function(Ajax) {
 
-    /**
-     * Get the list of items that the user has most recently accessed.
-     *
-     * @method getRecentItems
-     * @param {int} limit Only return this many results
-     * @return {promise} Resolved with an array of items
-     */
-    var getRecentItems = function(limit) {
-        var args = {};
-        if (typeof limit !== 'undefined') {
-            args.limit = limit;
-        }
-        var request = {
-            methodname: 'block_recentlyaccesseditems_get_recent_items',
-            args: args
-        };
-        return Ajax.call([request])[0];
-    };
-    return {
-        getRecentItems: getRecentItems
-    };
-});
+import {fetchOne} from 'core/fetch';
+
+/**
+ * Get the list of items that the user has most recently accessed.
+ *
+ * @method getRecentItems
+ * @param {Number} limit Only return this many results
+ * @return {Promise} Resolved with an array of items
+ */
+export const getRecentItems = (limit) => {
+    const args = {};
+    if (limit) {
+        args.limit = limit;
+    }
+
+    return fetchOne(
+        'block_recentlyaccesseditems_get_recent_items',
+        args
+    );
+};
