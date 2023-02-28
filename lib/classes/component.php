e3a941513a6b0f7793c99b11b2db3b29252a1ba4
Alex Morris <alex.morris@catalyst.net.nz>
2023-02-28 18:13:10 +1300

    MDL-76722 lib: Add phpseclib library

--- lib/classes/component.php
+++ lib/classes/component.php
@@ -116,6 +116,7 @@ class core_component {
         'GuzzleHttp\\Promise' => 'lib/guzzlehttp/promises/src',
         'GuzzleHttp' => 'lib/guzzlehttp/guzzle/src',
         'Kevinrob\\GuzzleCache' => 'lib/guzzlehttp/kevinrob/guzzlecache/src',
+        'phpseclib3' => 'lib/phpseclib/src',
     );
 
     /**
---
+++ lib/phpseclib/readme_moodle.txt
@@ -0,0 +1,7 @@
+Instructions to import phpseclib into Moodle:
+
+1/ Download from https://github.com/phpseclib/phpseclib/releases
+
+2/ Copy the LICENSE file into the lib/phpseclib folder
+
+3/ Copy the phpseclib folder into the lib/phpseclib/src folder
