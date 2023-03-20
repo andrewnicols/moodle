bf1d78348b714b8a6f65cff7af03e7e7ff56f8b6
Andrew Nicols <andrew@nicols.co.uk>
2023-03-20 14:31:55 +0800

    Accept insecure certs

--- lib/behat/classes/behat_config_util.php
+++ lib/behat/classes/behat_config_util.php
@@ -552,7 +552,15 @@ class behat_config_util {
                 'extensions' => array(
                     'Behat\MinkExtension' => array(
                         'base_url' => $CFG->behat_wwwroot,
-                        'goutte' => null,
+                        'goutte' => [
+                            // Do not validate certificate security.
+                            // This is required for situations where we test things like oauth.
+                            'guzzle_parameters' => [
+                                'defaults' => [
+                                    'verify' => false,
+                                ],
+                            ],
+                        ],
                         'webdriver' => $webdriverwdhost
                     ),
                     'Moodle\BehatExtension' => array(
