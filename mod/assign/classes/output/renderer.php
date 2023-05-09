ac9953ad31f92b2310da3a2404e9a6a157899a9f
Andrew Nicols <andrew@nicols.co.uk>
2023-05-09 13:37:00 +0800

    MDL-78168 mod_assign: Stop storing properties dynamically
    In PHP 8.2 and later, setting a value to an undeclared class property is
    deprecated and emits a deprecation notice.
    So we need to add missing class properties that still need to be declared.
    
    In these cases, mod_assign was treating other unrelated objects as its
    personal object store. These have been updated to not require this.

--- mod/assign/classes/output/renderer.php
+++ mod/assign/classes/output/renderer.php
@@ -1397,11 +1397,13 @@ class renderer extends \plugin_renderer_base {
             $result .= '<li yuiConfig=\'' . json_encode($yuiconfig) . '\'>' .
                 '<div>' .
                     '<div class="fileuploadsubmission">' . $image . ' ' .
-                    $file->fileurl . ' ' .
+                    html_writer::link($tree->get_file_url($file), $file->get_filename(), [
+                        'target' => '_blank',
+                    ]) . ' ' .
                     $plagiarismlinks . ' ' .
-                    $file->portfoliobutton . ' ' .
+                    $this->get_portfolio_button($tree, $file) . ' ' .
                     '</div>' .
-                    '<div class="fileuploadsubmissiontime">' . $file->timemodified . '</div>' .
+                    '<div class="fileuploadsubmissiontime">' . $tree->get_modified_time($file) . '</div>' .
                 '</div>' .
             '</li>';
         }
@@ -1409,6 +1411,35 @@ class renderer extends \plugin_renderer_base {
         $result .= '</ul>';
 
         return $result;
+    }
+
+    /**
+     * Get the portfolio button content for the specified file.
+     *
+     * @param assign_filed $tree
+     * @param stored_file $file
+     * @return string
+     */
+    protected function get_portfolio_button(assign_files $tree, stored_file $file): string {
+        global $CFG;
+        if (empty($CFG->enableportfolios)) {
+            return '';
+        }
+
+        if (!has_capability('mod/assign:exportownsubmission', $tree->context)) {
+            return '';
+        }
+
+        require_once($CFG->libdir . '/portfoliolib.php');
+
+        $button = new portfolio_add_button();
+        $portfolioparams = [
+            'cmid' => $tree->cm->id,
+            'fileid' => $file->get_id(),
+        ];
+        $button->set_callback_options('assign_portfolio_caller', $portfolioparams, 'mod_assign');
+        $button->set_format_by_file($file);
+        return $button->to_html(PORTFOLIO_ADD_ICON_LINK);
     }
 
     /**
