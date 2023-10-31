1d56b04c3cd0e3f16fba61cd116044efb53daa40
Andrew Nicols <andrew@nicols.co.uk>
2023-10-31 10:42:26 +0800

    MDL-76834 core_course: Migrate /course/view to routed API

---
+++ course/classes/route/shim/course_routes.php
@@ -0,0 +1,79 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace core_course\route\shim;
+
+use core\param;
+use core\router\schema\parameters\query_parameter;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use core\router\route;
+
+class course_routes {
+    use \core\router\route_controller;
+
+    #[route(
+        path: '/view.php',
+        queryparams: [
+            new query_parameter(
+                name: 'id',
+                type: param::INT,
+                description: 'The course ID',
+            ),
+            new query_parameter(
+                name: 'idnumber',
+                type: param::RAW,
+                description: 'The course idnumber',
+            ),
+            new query_parameter(
+                name: 'name',
+                type: param::TEXT,
+                description: 'The course shortname',
+            ),
+        ],
+    )]
+    public function view_course(
+        ServerRequestInterface $request,
+        ResponseInterface $response,
+    ): ResponseInterface {
+        if ($this->get_param($request, 'id')) {
+            return $this->redirect_to_callable(
+                $request,
+                $response,
+                \core_course\route\controller\view_controller::class . '::view_course',
+                excludeparams: ['id'],
+            );
+        }
+        if ($this->get_param($request, 'idnumber')) {
+            return $this->redirect_to_callable(
+                $request,
+                $response,
+                \core_course\route\controller\view_controller::class . '::view_by_idnumber',
+                excludeparams: ['idnumber'],
+            );
+        }
+        if ($this->get_param($request, 'name')) {
+            return $this->redirect_to_callable(
+                $request,
+                $response,
+                \core_course\route\controller\view_controller::class . '::view_by_shortname',
+                excludeparams: ['name'],
+            );
+        }
+
+        return $this->page_not_found($request, $response);
+    }
+}
