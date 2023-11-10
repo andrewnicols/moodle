ea8d36d4f483862a320747f7ad693440970dc7f1
Andrew Nicols <andrew@nicols.co.uk>
2023-11-10 12:22:10 +0800

    MDL-80072 core: Move format_string to core\formatting

--- lib/weblib.php
+++ lib/weblib.php
@@ -1494,91 +1494,16 @@ function reset_text_filters_cache($phpunitreset = false) {
  * @return string
  */
 function format_string($string, $striplinks = true, $options = null) {
-    global $CFG, $PAGE;
+    global $CFG;
 
-    if ($string === '' || is_null($string)) {
-        // No need to do any filters and cleaning.
-        return '';
-    }
+    // Manually include the formatter class for now until after the release after 4.5 LTS.
+    require_once("{$CFG->libdir}/classes/formatter.php");
 
-    // We'll use a in-memory cache here to speed up repeated strings.
-    static $strcache = false;
-
-    if (empty($CFG->version) or $CFG->version < 2013051400 or during_initial_install()) {
-        // Do not filter anything during installation or before upgrade completes.
-        return $string = strip_tags($string);
-    }
-
-    if ($strcache === false or count($strcache) > 2000) {
-        // This number might need some tuning to limit memory usage in cron.
-        $strcache = array();
-    }
-
-    if (is_numeric($options)) {
-        // Legacy courseid usage.
-        $options  = array('context' => context_course::instance($options));
-    } else {
-        // Detach object, we can not modify it.
-        $options = (array)$options;
-    }
-
-    if (empty($options['context'])) {
-        // Fallback to $PAGE->context this may be problematic in CLI and other non-standard pages :-(.
-        $options['context'] = $PAGE->context;
-    } else if (is_numeric($options['context'])) {
-        $options['context'] = context::instance_by_id($options['context']);
-    }
-    if (!isset($options['filter'])) {
-        $options['filter'] = true;
-    }
-
-    $options['escape'] = !isset($options['escape']) || $options['escape'];
-
-    if (!$options['context']) {
-        // We did not find any context? weird.
-        return $string = strip_tags($string);
-    }
-
-    // Calculate md5.
-    $cachekeys = array($string, $striplinks, $options['context']->id,
-        $options['escape'], current_language(), $options['filter']);
-    $md5 = md5(implode('<+>', $cachekeys));
-
-    // Fetch from cache if possible.
-    if (isset($strcache[$md5])) {
-        return $strcache[$md5];
-    }
-
-    // First replace all ampersands not followed by html entity code
-    // Regular expression moved to its own method for easier unit testing.
-    $string = $options['escape'] ? replace_ampersands_not_followed_by_entity($string) : $string;
-
-    if (!empty($CFG->filterall) && $options['filter']) {
-        $filtermanager = filter_manager::instance();
-        $filtermanager->setup_page_for_filters($PAGE, $options['context']); // Setup global stuff filters may have.
-        $string = $filtermanager->filter_string($string, $options['context']);
-    }
-
-    // If the site requires it, strip ALL tags from this string.
-    if (!empty($CFG->formatstringstriptags)) {
-        if ($options['escape']) {
-            $string = str_replace(array('<', '>'), array('&lt;', '&gt;'), strip_tags($string));
-        } else {
-            $string = strip_tags($string);
-        }
-    } else {
-        // Otherwise strip just links if that is required (default).
-        if ($striplinks) {
-            // Strip links in string.
-            $string = strip_links($string);
-        }
-        $string = clean_text($string);
-    }
-
-    // Store to cache.
-    $strcache[$md5] = $string;
-
-    return $string;
+    return \core\container::get(\core\formatter::class)->format_string(
+        $string,
+        $striplinks,
+        $options,
+    );
 }
 
 /**
