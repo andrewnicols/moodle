c2a1d982dd783990dbb59925992600b26184345a
Andrew Nicols <andrew@nicols.co.uk>
2024-05-16 14:19:16 +0800

    MDL-81919 core: Move moodle_exception to autoloaded class

---
+++ lib/db/legacyclasses.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file contains mappings for legacy classes that do not fit the standard class naming conventions.
+ *
+ * In time these classes should be renamed to fit the standard class naming conventions but this is not an overnight process.
+ *
+ * @package    core
+ * @copyright  Andrew Lyons <andrew@nicols.co.uk>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+// Like other files in the db directory this file uses an array.
+// The old class name is the key, the path to the file containing the class is the vlaue.
+// The array must be called $legacyclasses.
+$legacyclasses = [
+    \moodle_exception::class => 'exception/moodle_exception.php',
+];
