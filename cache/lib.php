4fe0a616e60efcf719a53acda98ba132e391e1b3
Andrew Nicols <andrew@nicols.co.uk>
2024-06-11 14:11:54 +0800

    MDL-82158 core: Remove uses of cache/lib.php

--- cache/lib.php
+++ cache/lib.php
@@ -13,22 +13,3 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
-/**
- * The core cache API.
- *
- * Pretty much just includes the mandatory classes and contains the misc classes that arn't worth separating into individual files.
- *
- * This file is part of Moodle's cache API, affectionately called MUC.
- * It contains the components that are requried in order to use caching.
- *
- * @package    core
- * @category   cache
- * @copyright  2012 Sam Hemelryk
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-
-defined('MOODLE_INTERNAL') || die();
-
-// Include the required classes.
-require_once($CFG->dirroot.'/cache/classes/definition.php');
