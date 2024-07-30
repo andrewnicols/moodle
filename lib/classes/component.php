b78756bbdf5358d1f424d97befb636aaf563760c
Andrew Nicols <andrew@nicols.co.uk>
2024-07-31 07:29:10 +0800

    fixup! MDL-81031 core: Add Slim Framework

--- lib/classes/component.php
+++ lib/classes/component.php
@@ -146,7 +146,6 @@ class component {
         \PhpOffice\PhpSpreadsheet::class => 'lib/phpspreadsheet/phpspreadsheet/src/PhpSpreadsheet',
         \Invoker::class => 'lib/php-di/invoker/src',
         \FastRoute::class => 'lib/nikic/fast-route/src',
-        \DI\\Bridge\\Slim::class => "lib/php-di/slim-bridge/src",
         \Slim::class => 'lib/slim/slim/Slim',
     ];
 
