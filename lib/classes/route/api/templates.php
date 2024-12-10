6ff6acfb86398d2ef70314efaf793a72d385c703
Andrew Nicols <andrew@nicols.co.uk>
2024-12-10 11:15:55 +0800

    MDL-82652 core: Allow routed templates to be cached

--- lib/classes/route/api/templates.php
+++ lib/classes/route/api/templates.php
@@ -49,6 +49,7 @@ class templates {
         title: 'Fetch a single template',
         description: 'Fetch a single template for a component in a theme',
         security: [],
+        cachelifetime: '90 days',
         pathtypes: [
             new \core\router\parameters\path_themename(),
             new \core\router\parameters\path_component(),
