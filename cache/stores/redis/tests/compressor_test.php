f843d1078d419b7742d08ccdddd3213768d2c788
Andrew Nicols <andrew@nicols.co.uk>
2025-01-16 09:59:47 +0800

    MDL-83468 cachestore_redis: Skip tests if Redis extension not available

--- cache/stores/redis/tests/compressor_test.php
+++ cache/stores/redis/tests/compressor_test.php
@@ -234,6 +234,7 @@ final class compressor_test extends \advanced_testcase {
      * Test we can use get and set with all combinations.
      *
      * @dataProvider provider_for_tests_setget
+     * @requires extension Redis
      * @param string $name
      * @param int $serializer
      * @param int $compressor
@@ -258,6 +259,7 @@ final class compressor_test extends \advanced_testcase {
      * Test we can use get and set many with all combinations.
      *
      * @dataProvider provider_for_tests_setget
+     * @requires extension Redis
      * @param string $name
      * @param int $serializer
      * @param int $compressor
