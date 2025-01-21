b97b95ac9bb369958e77444785bb3281ccf05800
Andrew Nicols <andrew@nicols.co.uk>
2025-01-21 13:29:08 +0800

    MDL-84204 core: Add composer-bin for dev dependencies

--- .grunt/components.js
+++ .grunt/components.js
@@ -203,7 +203,7 @@ const getThirdPartyPaths = () => {
     const xpath = require('xpath');
 
     const thirdpartyfiles = getThirdPartyLibsList(fs.realpathSync('./'));
-    const libs = ['node_modules/', 'vendor/'];
+    const libs = ['node_modules/', 'vendor/', 'vendor-bin/'];
 
     const addLibToList = lib => {
         if (!lib.match('\\*') && fs.statSync(lib).isDirectory()) {
