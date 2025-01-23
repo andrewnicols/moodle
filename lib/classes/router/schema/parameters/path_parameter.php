dcde80e1babf7caa2483e760e9f7f10d9d604e79
Andrew Nicols <andrew@nicols.co.uk>
2025-01-23 11:41:58 +0800

    MDL-84300 router: Path params should be aware of unlimited params
    Where a path contains an unlimited parameter, designated by `{name:.*}`
    or `{name:.*?}` it may be empty and therefore cannot be required.

--- lib/classes/router/schema/parameters/path_parameter.php
+++ lib/classes/router/schema/parameters/path_parameter.php
@@ -103,11 +103,21 @@ class path_parameter extends parameter {
     public function is_required(route $route): bool {
         $path = $route->get_path();
 
-        // Find the position of the parameter in the path.
-        $paramposition = strpos($path, '{' . $this->name . '}');
+        // Find the parameter in the path.
+        // Search for `{value` with an optoinal : followed by anything except a closing `}`, and then a closing `}`.
+        $matchesfound = preg_match(
+            "~^(?<match>.*\{{$this->name}(?:\:[^}]*)?\}\}?)~",
+            $path,
+            $matches,
+        );
+
+        if ($matchesfound === 0) {
+            // Parameter not found in the path.
+            return false;
+        }
 
         // If _any_ part of the path before the parameter contains a '[' character, then this _must_ be optional.
         // A required parameter cannot follow an optional parameter.
-        return !str_contains(substr($path, 0, $paramposition), '[');
+        return str_contains($matches['match'], '[') === false;
     }
 }
