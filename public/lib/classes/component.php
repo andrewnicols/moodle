afad14bf181e1a19d8b6b5ceca7e5842ebba830b
Andrew Nicols <andrew@nicols.co.uk>
2025-06-25 15:43:34 +0800

    MDL-85847 core: Add support for Symfony Console

--- public/lib/classes/component.php
+++ public/lib/classes/component.php
@@ -203,6 +203,9 @@ class component {
      * Register the Moodle class autoloader.
      */
     public static function register_autoloader(): void {
+        if (is_file(dirname(__DIR__, 3) . '/vendor/autoload.php')) {
+            require_once(dirname(__DIR__, 3) . '/vendor/autoload.php');
+        }
         if (defined('COMPONENT_CLASSLOADER')) {
             spl_autoload_register(COMPONENT_CLASSLOADER);
         } else {
@@ -1452,7 +1455,6 @@ $cache = ' . var_export($cache, true) . ';
      * and $namespace is empty.
      */
     public static function get_component_classes_in_namespace($component = null, $namespace = '') {
-
         $classes = [];
         self::init();
 
@@ -1481,6 +1483,21 @@ $cache = ' . var_export($cache, true) . ';
         }
 
         return $classes;
+    }
+
+    public static function get_classes_matching_namespace(string $namespace): array {
+        $classes = [];
+        foreach (self::get_component_names(true) as $component) {
+            $classes = array_merge(
+                $classes,
+                component::get_component_classes_in_namespace(
+                    component: $component,
+                    namespace: $namespace,
+                ),
+            );
+        }
+
+        return array_keys($classes);
     }
 
     /**
