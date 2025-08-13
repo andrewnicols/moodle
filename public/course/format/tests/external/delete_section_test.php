d04f0495216e9d686a75c60a5099ff5eb2aa6437
Andrew Nicols <andrew@nicols.co.uk>
2025-08-13 11:26:54 +0800

    MDL-86283 courseformat: Correct erroneous coverage

--- public/course/format/tests/external/delete_section_test.php
+++ public/course/format/tests/external/delete_section_test.php
@@ -33,21 +33,17 @@ require_once($CFG->dirroot . '/webservice/tests/helpers.php');
  * @copyright  2025 Laurent David <laurent.david@moodle.com>
  * @category   test
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- * @coversDefaultClass \core_courseformat\stateactions
  */
+#[\PHPUnit\Framework\Attributes\CoversClass(stateactions::class)]
 final class delete_section_test extends \externallib_advanced_testcase {
-
     /**
      * Test the webservice can execute the section_delete action.
      *
-     * @covers ::section_delete
-     * @dataProvider section_delete_provider
      * @param int $sectionum
      * @param string $format
      * @param int $expectedsectionum
-     *
-     * @throws \moodle_exception
      */
+    #[\PHPUnit\Framework\Attributes\DataProvider('section_delete_provider')]
     public function test_delete_section(int $sectionum, string $format, int $expectedsectionum): void {
         $this->resetAfterTest();
 
