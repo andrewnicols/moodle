cbc450edb3c31ee5b1c197492abe0dcd2671a1d4
Andrew Nicols <andrew@nicols.co.uk>
2026-03-16 09:12:23 +0800

    MDL-88219 core: Update to latest AWS-SDk version

--- public/lib/aws-sdk/src/Signature/SignatureV4.php
+++ public/lib/aws-sdk/src/Signature/SignatureV4.php
@@ -155,6 +155,9 @@ class SignatureV4 implements SignatureInterface
                 $presignHeaders[] = $lName;
             }
         }
+
+        sort($presignHeaders);
+
         return $presignHeaders;
     }
 
@@ -346,7 +349,9 @@ class SignatureV4 implements SignatureInterface
         }
 
         $qs = '';
-        ksort($query);
+        uksort($query, static function (string $a, string $b): int {
+            return strcmp(rawurlencode($a), rawurlencode($b));
+        });
         foreach ($query as $k => $v) {
             if (!is_array($v)) {
                 $qs .= rawurlencode($k) . '=' . rawurlencode($v !== null ? $v : '') . '&';
