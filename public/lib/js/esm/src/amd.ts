755d31864a645979edabe497ccf6955af849e9b9
Andrew Nicols <andrew@nicols.co.uk>
2026-05-12 11:57:32 +0800

    Add notes from code review
    - We should define `getString()`, not `getStrings()` because we do not
      intend to add that in our final release
    - We should not be testing the internals of how the string handler loads
      strings - that's an implementation detail
    - We need to mock AMD modules
    - We need to throw an exception if we have not _explicitly_ mocked an
      AMD module
    - We need to have a default implementation for core/str because it is
      very widely used

--- public/lib/js/esm/src/amd.ts
+++ public/lib/js/esm/src/amd.ts
@@ -21,6 +21,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+/* istanbul ignore file */
+
 /**
  * Load a single AMD module and return it as a native Promise.
  *
