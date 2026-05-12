bce7d89f6079712b515ac725f1af350c7090a4c6
Andrew Nicols <andrew@nicols.co.uk>
2026-05-12 15:46:23 +0800

    MDL-88762 core: Convert core/config to esm

--- public/lib/classes/output/requirements/page_requirements_manager.php
+++ public/lib/classes/output/requirements/page_requirements_manager.php
@@ -1581,6 +1581,7 @@ EOF;
         \core\hook\output\before_requirejs_config $requirejshook,
     ): string {
         $requirejshook->add_requirejs_map_entries([
+            'core/config' => '@moodle/lms/core/config:default',
             'core/pending' => '@moodle/lms/core/pending:default',
         ]);
 
