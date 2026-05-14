1380c84d474045b723c472fa41f0a146a2e222c8
Andrew Nicols <andrew@nicols.co.uk>
2026-05-14 10:31:53 +0800

    MDL-XXXXX core: Convert core/url to ESM

--- public/lib/classes/output/requirements/page_requirements_manager.php
+++ public/lib/classes/output/requirements/page_requirements_manager.php
@@ -1585,6 +1585,7 @@ EOF;
             'core/pending' => '@moodle/lms/core/pending:default',
             'core/sessionstorage' => '@moodle/lms/core/SessionStorage:default',
             'core/storagewrapper' => '@moodle/lms/core/StorageWrapper:default',
+            'core/url' => '@moodle/lms/core/url',
             'core/utils' => '@moodle/lms/core/utils',
         ];
 
