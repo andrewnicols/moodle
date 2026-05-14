a1880c0fda3cebfeba3ecd52217a929a64bc6c04
Andrew Nicols <andrew@nicols.co.uk>
2026-05-14 20:27:36 +0800

    fixup! BUILD

---
+++ public/lib/js/esm/build/location.js
@@ -0,0 +1,10 @@
+function n(i){window.location.assign(i)}export{n as redirect};
+/**
+ * Browser location utilities.
+ *
+ * Provides a mockable abstraction over `window.location` for navigation actions.
+ *
+ * @module     core/location
+ * @copyright  2025 Andrew Lyons <andrew@nicols.co.uk>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
