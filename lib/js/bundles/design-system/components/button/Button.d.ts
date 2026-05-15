6d6b7a3994262edba510d4a9b4e9f6448f4ab956
Andrew Nicols <andrew@nicols.co.uk>
2026-05-15 14:02:40 +0800

    Bump MDS

--- lib/js/bundles/design-system/components/button/Button.d.ts
+++ lib/js/bundles/design-system/components/button/Button.d.ts
@@ -1,7 +1,12 @@
-import { ButtonHTMLAttributes } from 'react';
+import { ButtonHTMLAttributes, ReactElement } from 'react';
+type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'outline-primary' | 'outline-secondary' | 'outline-danger';
+type IconElement = ReactElement<'i' | 'svg'>;
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-    label: string;
-    variant?: string;
+    label?: string;
+    variant?: ButtonVariant;
     size?: 'sm' | 'lg';
+    startIcon?: IconElement;
+    endIcon?: IconElement;
 }
-export declare const Button: ({ label, variant, size, className, type, ...props }: ButtonProps) => import("react/jsx-runtime").JSX.Element;
+export declare const Button: ({ label, variant, size, startIcon, endIcon, className, type, ...props }: ButtonProps) => import("react/jsx-runtime").JSX.Element;
+export {};
