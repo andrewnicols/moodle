3b7594fac39deb612bca26199e90fa594ddfa14f
Andrew Nicols <andrew@nicols.co.uk>
2026-07-15 11:31:12 +0800

    MDL-89196 core: Update login renderable
    The login renderable had a bunch of public properties which make
    updating it harder.
    
    We will be doing some updates to this to have it share properties with
    similar pages as part of our OAuth2 system. To make this easier the old
    login renderable has been removed in favour of a new loginform
    renderable. This matches the naming of the current template.
    
    This has not been deprecated because it is not expected to be a public
    API. Whilst themes may extend it should only be called by core which
    explicitly calls the new version. There is no codepath whereby the old
    version can be called.
    
    By removing it instead of modifying or deprecating it we allow themes to
    support theming of both the <= 5.2 version, and the >= 5.3 version.

--- public/login/index.php
+++ public/login/index.php
@@ -408,7 +408,7 @@ if (isloggedin() and !isguestuser()) {
     );
     echo $OUTPUT->box_end();
 } else {
-    $loginform = new \core_auth\output\login($authsequence, $frm->username);
+    $loginform = new \core_auth\output\login_form($authsequence, $frm->username);
     $loginform->set_error($errormsg, $errorcode);
     $loginform->set_info($infomsg);
     echo $OUTPUT->render($loginform);
