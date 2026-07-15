3bcb0c6101403e699bdf05c84369bacc2848d110
Andrew Nicols <andrew@nicols.co.uk>
2026-07-15 11:31:12 +0800

    MDL-89196 core: Update login renderable
    The login renderable had a bunch of public properties which make
    updating it harder.
    
    We will be doing some updates to this to have it share properties with
    similar pages as part of our OAuth2 system. To make this easier the old
    login renderable has been removed in favour of a new loginform
    renderable. This matches the naming of the current template.
    
    This has not been deprecated because it is not expected to be a public
    API. Whilst themes may extend it should only be called by core which
    explicitly calls the new version. There is no codepath whereby the old
    version can be called.
    
    By removing it instead of modifying or deprecating it we allow themes to
    support theming of both the <= 5.2 version, and the >= 5.3 version.

---
+++ public/auth/classes/output/login_form.php
@@ -0,0 +1,159 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace core_auth\output;
+
+use core\output\help_icon;
+use core\url;
+
+/**
+ * Login renderable class.
+ *
+ * @package    core_auth
+ * @copyright  Andrew Lyons <andrew@nicols.co.uk>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class login_form implements
+    \core\output\named_templatable,
+    \core\output\renderable
+{
+    use login_renderable_trait {
+        export_for_template as shared_export_for_template;
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param array $authsequence The enabled sequence of authentication plugins.
+     * @param string $username The username to display.
+     */
+    public function __construct(
+        /** @var string[] The order of authentication plugins */
+        private array $authsequence,
+        /** @var string The user name to pre-fill the form with. */
+        private string $username = '',
+    ) {
+    }
+
+    #[\Override]
+    public function get_template_name(\core\output\renderer_base $renderer): string {
+        return 'core/loginform';
+    }
+
+    /**
+     * Export data for the template
+     *
+     * @param \core\output\renderer_base $output
+     * @return \stdClass
+     */
+    public function export_for_template(\core\output\renderer_base $output): \stdClass {
+        global $CFG;
+
+        $data = $this->shared_export_for_template($output);
+
+        $data->logintoken = \core\session\manager::get_login_token();
+
+        $data->autofocusform = !empty($CFG->loginpageautofocus);
+
+        $data->canloginasguest = $CFG->guestloginbutton && !isguestuser();
+        $data->canloginbyemail = !empty($CFG->authloginviaemail);
+        $data->cansignup = $CFG->registerauth == 'email' || !empty($CFG->registerauth);
+
+        $data->cookieshelpicon = $this->get_cookies_help_icon()->export_for_template($output);
+
+        $data->forgotpasswordurl = new url('/login/forgot_password.php');
+        $loginurl = new url('/login/index.php');
+        $signupurl = new url('/login/signup.php');
+        $data->loginurl = $loginurl->out(false);
+        $data->signupurl = $signupurl->out(false);
+
+        // Authentication instructions.
+        $data->instructions = $CFG->auth_instructions;
+        if (\core\di::get(\core\authentication::class)->is_enabled('none')) {
+            $data->instructions = get_string('loginstepsnone');
+        } else if ($CFG->registerauth == 'email' && empty($data->instructions)) {
+            $data->instructions = get_string('logindonthaveaccount');
+        }
+
+        // ReCaptcha.
+        $data->recaptcha = $this->get_recaptcha();
+
+        // Toggle password visibility icon.
+        $data->togglepassword = $this->get_toggle_password();
+        $data->smallscreensonly = get_config('core', 'loginpasswordtoggle') == TOGGLE_SENSITIVE_SMALL_SCREENS_ONLY;
+
+        $data->forgotpasswordurl = $data->forgotpasswordurl->out(false);
+
+        // Identity Providers.
+        $identityproviders = \auth_plugin_base::get_identity_providers($this->authsequence);
+        $data->identityproviders = \auth_plugin_base::prepare_identity_providers_for_output($identityproviders, $output);
+        $data->hasidentityproviders = !empty($data->identityproviders);
+
+        // Login instructions.
+        [$data->instructions, $data->instructionsformat] = \core_external\util::format_text(
+            $data->instructions,
+            FORMAT_MOODLE,
+            \core\context\system::instance()->id,
+        );
+        $data->username = $this->username;
+        $data->showloginform = get_config('core', 'showloginform') === false || get_config('core', 'showloginform');
+
+        $data->hasauthinstructions = !empty($CFG->auth_instructions);
+
+        return $data;
+    }
+
+    /**
+     * Can the user toggle password visibility.
+     *
+     * @return bool
+     */
+    protected function get_toggle_password(): bool {
+        $value = get_config('core', 'loginpasswordtoggle');
+        return $value == TOGGLE_SENSITIVE_ENABLED || $value == TOGGLE_SENSITIVE_SMALL_SCREENS_ONLY;
+    }
+
+    /**
+     * Get the ReCaptcha HTML if enabled.
+     *
+     * @return string|null
+     */
+    protected function get_recaptcha(): ?string {
+        global $CFG;
+
+        if (login_captcha_enabled()) {
+            require_once($CFG->libdir . '/recaptchalib_v2.php');
+            return recaptcha_get_challenge_html(RECAPTCHA_API_URL, $CFG->recaptchapublickey);
+        }
+
+        return null;
+    }
+
+    /**
+     * Get the help icon for the Cookies helper.
+     *
+     * @return help_icon
+     */
+    protected function get_cookies_help_icon(): help_icon {
+        global $CFG;
+
+        if ($CFG->rememberusername == 0) {
+            return new help_icon('cookiesenabledonlysession', 'core');
+        } else {
+            return new help_icon('cookiesenabled', 'core');
+        }
+    }
+}
---
+++ public/auth/classes/output/login_renderable_trait.php
@@ -0,0 +1,144 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace core_auth\output;
+
+/**
+ * Trait to share login renderable properties between the standard login renderable and the SSO login renderable.
+ *
+ * @package    core_auth
+ * @copyright  2026 Andrew Lyons <andrew@nicols.co.uk>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+trait login_renderable_trait {
+    /** @var string The error message, if any. */
+    private ?string $error = null;
+
+    /** @var string The error title, shown as bold heading above the error message for credential failures. */
+    private ?string $errortitle = null;
+
+    /** @var string The info message, if any. */
+    private ?string $info = null;
+
+    /**
+     * Set the error message. For the AUTH_LOGIN_FAILED case, also sets
+     * an errortitle so the template can render a bold heading above the detail text.
+     *
+     * @param string $error The error message.
+     * @param int $errorcode The error code from login/index.php.
+     */
+    public function set_error(string $error, int $errorcode = 0): void {
+        if ($errorcode === AUTH_LOGIN_FAILED) {
+            $this->errortitle = get_string('logininvalidlogintitle');
+            $this->error = get_string('logininvalidlogindetail');
+        } else {
+            $this->error = $error;
+        }
+    }
+
+    /**
+     * Set the info message.
+     *
+     * @param string $info The info message.
+     */
+    public function set_info(string $info): void {
+        $this->info = $info;
+    }
+
+    /**
+     * Export data for the template
+     *
+     * @param \core\output\renderer_base $output
+     * @return \stdClass
+     */
+    public function export_for_template(\core\output\renderer_base $output): \stdClass {
+        return (object) [
+            'error' => $this->error,
+            'errorformatted' => $output->error_text($this->error),
+            'errortitle' => $this->errortitle,
+            'info' => $this->info,
+            'languagemenu' => $this->get_language_menu($output),
+            'maintenance' => \format_text($this->get_maintenance_message(), FORMAT_MOODLE),
+            'sitename' => $this->get_site_name(),
+            'logourl' => $this->get_logo_url(),
+        ];
+    }
+
+    /**
+     * Get the language menu data for the template.
+     *
+     * @param \core\output\renderer_base $output
+     * @return \stdClass|null
+     */
+    protected function get_language_menu(\core\output\renderer_base $output): ?\stdClass {
+        global $PAGE;
+
+        $languagedata = new \core\output\language_menu($PAGE);
+        return $languagedata->export_for_action_menu($output);
+    }
+
+    /**
+     * Get the site name, formatted for display.
+     *
+     * @return string The formatted site name.
+     */
+    protected function get_site_name(): string {
+        global $SITE;
+
+        $formatter = \core\di::get(\core\formatting::class);
+
+        return $formatter->format_string(
+            string: $SITE->fullname,
+            context: \core\context\course::instance(SITEID),
+            escape: false,
+        );
+    }
+
+    /**
+     * Get the logo URL, if any.
+     *
+     * @return string|null The logo URL, or null if no logo is set.
+     */
+    protected function get_logo_url(): ?string {
+        global $PAGE;
+
+        $logourl = $PAGE->get_renderer('core')->get_logo_url();
+        if ($logourl) {
+            return $logourl->out(false);
+        }
+
+        return null;
+    }
+
+    /**
+     * Get the maintenance message, if any.
+     *
+     * @return null|string The maintenance message, or null if maintenance mode is not enabled.
+     */
+    protected function get_maintenance_message(): ?string {
+        global $CFG;
+
+        if ($CFG->maintenance_enabled == true) {
+            if (!empty($CFG->maintenance_message)) {
+                return $CFG->maintenance_message;
+            } else {
+                return get_string('sitemaintenance', 'admin');
+            }
+        }
+
+        return null;
+    }
+}
