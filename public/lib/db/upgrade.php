04d5e5971074e9b2573337de5927578d848a9626
Andrew Nicols <andrew@nicols.co.uk>
2026-07-16 11:27:52 +0800

    MDL-88472 oauth2: Set up OAuth2 Keys

--- public/lib/db/upgrade.php
+++ public/lib/db/upgrade.php
@@ -2159,6 +2159,9 @@ function xmldb_main_upgrade($oldversion) {
             $dbman->create_table($table);
         }
 
+        // Generate the keys for oauth2.
+        \core\di::get(\core\oauth2\setup::class)->configure_keys();
+
         // Main savepoint reached.
         upgrade_main_savepoint(true, 2026071800.00);
     }
